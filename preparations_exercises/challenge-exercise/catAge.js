25924004b59826c094a8808fbecf98d5a6f25cf1
Irina Mityugova <info@irinamityugova.me>
2022-12-31 23:43:36 +0000

    Optimizing

--- preparations_exercises/challenge-exercise/catAge.js
+++ preparations_exercises/challenge-exercise/catAge.js
@@ -1,27 +1,18 @@
 function catAge (catAge) {
-  let humanAge = 0;
-
-  for (let i = 0; i <= catAge; i++) {
-    switch (i) {
-      case 0:
-        humanAge+=0;
-        continue;
-      case 1:
-        humanAge+=15;
-        continue;
-      case 2:
-        humanAge+=9;
-        continue;
-      default:
-        humanAge+=4;
-    }
+  switch (catAge) {
+    case 0:
+      return 0;
+    case 1:
+      return 15;
+    case 2:
+      return 24;
+    default:
+      return 24 + (catAge - 2) * 4;
   }
-  console.log(humanAge)
-  return humanAge;
 }
 
 catAge(0); // 0
 catAge(1); // 15
 catAge(2); // 24
 catAge(3); // 28
-catAge(4); // 32
+console.log(catAge(4)); // 32
