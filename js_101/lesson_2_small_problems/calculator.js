b992fec7a266f14d0443464433cb7eb1ab7f3190
Irina Mityugova <info@irinamityugova.me>
2023-01-13 17:33:10 +0000

    JS 101: Lesson 2 - Refactoring Calculator for ESlint

--- js_101/lesson_2_small_problems/calculator.js
+++ js_101/lesson_2_small_problems/calculator.js
@@ -1,99 +1,111 @@
-/*
-Create a calculator
+/**
+ * Create a calculator
+ *
+ * Pseudocode:
+ * Ask the user for a number
+ * Ask the user for the operation
+ * Ask the user for the second number
+ * Conduct the operation
+ * Return the result
+ * Ask the user to continue with another operation or cancel the result to 0
+ * Escape the application with a word "exit" at any point
+ *
+ * Test cases:
+ * - Simple algorithmic expressions (+, -, *, /, ^, %)
+ * - Unknown operator
+ * - Division by 0
+ *
+ * Error handling:
+ * - Incorrect input
+ *
+ */
 
-Pseudocode:
-Ask the user for a number
-Ask the user for the operation
-Ask the user for the second number
-Conduct the operation
-Return the result
-Ask the user to continue with another operation or cancel the result to 0
-Escape the application with a word "exit" at any point
+const rlSync = require('readline-sync');
 
-Test cases:
-// Simple algorithmic expressions (+, -, *, /, ^, %)
-// Unknown operator
-// Division by 0
+const calculator = function calculator() {
+  console.log(`
+  |-------------------------------------------------|
+  |         Welcome to Console Calculator!          |
+  |-------------------------------------------------|
+  | Answer the following prompts to get the result. |
+  | You can reuse the result of operation.          |
+  | Press Ctrl-C to exit the application.           |
+  |-------------------------------------------------|`);
 
-Error handling:
-// Incorrect input
-// Division by 0
+  let result;
+  let input;
+  let num1;
+  let operation;
+  let num2;
+  let another;
+  let useResult;
 
-*/
+  const requestInput = function requestInputFromUser() {
+    if (result) {
+      do {
+        useResult = rlSync.question('Continue with a result? y/n ');
+      } while (!useResult.match(/[yn]/));
 
-const rlSync = require("readline-sync");
-
-console.log(`
-|-------------------------------------------------|
-|         Welcome to Console Calculator!          |
-|-------------------------------------------------|
-| Answer the following prompts to get the result. |
-| You can reuse the result of operation.          |
-| Press Ctrl-C to exit the application.           |
-|-------------------------------------------------|`);
-
-let num1 = rlSync.questionInt("Number: ");
-let operation = "";
-let num2 = 0;
-let another = "y";
-let result = 0;
-
-let produceResult = function (a, b, oprtn) {
-
-  if (result) {
-
-    let useResult = rlSync.question("Continue with a result? y/n ")
-
-    if (useResult === "n") {
-      result = 0;
-      a = rlSync.questionInt("Number: ");
-    } else if (useResult === "y"){
-      a = result;
+      switch (useResult) {
+        case 'y':
+          num1 = result;
+          break;
+        case 'n':
+          result = 0;
+          num1 = rlSync.questionInt('Number: ');
+          break;
+        default: console.log('Incorrect input');
+      }
     } else {
-      console.log("I'll assume you want to keep using the result... Please use valid y or n for an answer.")
-      a = result;
+      num1 = rlSync.questionInt('Number: ');
     }
-  }
+
+    do {
+      operation = rlSync.question('Operator: ');
+      if (!operation.match(/[+\-*/%^]/)) {
+        console.log(`
+|------------------------- ERROR ---------------------------|
+| ${operation} is not a registered mathematical operator.   |
+| Please select from the following commands:                |
+| + to add,                                                 |
+| - to subtract,                                            |
+| * to multiply,                                            |
+| / to divide,                                              |
+| % to get the remainder of the division,                   |
+| ^ to put previous number to the power of the next number. |
+|-----------------------------------------------------------|`);
+      }
+    } while (!operation.match(/[+\-*/%^]/));
+
+    num2 = rlSync.questionInt('Number: ');
+
+    input = {
+      num1,
+      num2,
+      operation,
+    };
+  };
+
+  const produceResult = function produceResultOfAnOperation(a, b, oprtn) {
+    switch (oprtn) {
+      case '+': result = a + b; break;
+      case '-': result = a - b; break;
+      case '*': result = a * b; break;
+      case '/': result = a / b; break;
+      case '%': result = a % b; break;
+      case '^': result = a ** b; break;
+      default:
+    }
+    console.log('Result =', result);
+    return result;
+  };
 
   do {
-    oprtn = rlSync.question("Operator: ");
-    if (!oprtn.match(/[+\-*/%^]/)) {
-      console.log(`
-        |-------------------------------- ERROR ----------------------------------|
-        | ${oprtn} is not registered.                                             |
-        | Please select from the following commands:                              |
-        | + to add,                                                               |
-        | - to subtract,                                                          |
-        | * to multiply,                                                          |
-        | / to divide,                                                            |
-        | % to get the remainder of the division,                                 |
-        | ^ to put previous number to the power of the next number.               |
-        |-------------------------------------------------------------------------|`)
-    }
-  } while (!oprtn.match(/[+\-*/%^]/))
+    requestInput(result);
+    produceResult(input.num1, input.num2, input.operation);
 
-  b = rlSync.questionInt("Number: ");
+    another = rlSync.question('Would you like to perform another operation? y/n ');
+  } while (another.match(/y/));
+};
 
-
-  switch (oprtn) {
-    case "exit": return;
-    case "+": result = a + b; break;
-    case "-": result = a - b; break;
-    case "*": result = a * b; break;
-    case "/": result = a / b; break;
-    case "%": result = a % b; break;
-    case "^": result = a ** b; break;
-    default:
-
-
-  }
-  return result;
-}
-
-// loop the application
-do {
-  result = produceResult(num1, num2, operation);
-  console.log("|-> Result:", result)
-
-  another = rlSync.question("Would you like to perform another operation? y/n ")
-} while (another.match(/y/))
+calculator();
