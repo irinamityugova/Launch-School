86358d4d17cf231f8980d7c9b9b204dca421d0dd
Irina Mityugova <info@irinamityugova.me>
2023-01-24 23:58:39 +0000

    Fibonacci Number Location By Length

--- js_101/exercises/easy-3.js
+++ js_101/exercises/easy-3.js
@@ -19,4 +19,66 @@ const removeDuplicates = (str) => {
   return result;
 };
 
-console.log(removeDuplicates ('abaa'));
+// console.log(removeDuplicates('abaa'));
+
+/**
+ * Stringy Strings
+ * Write a function that takes one argument, a positive integer, and returns a
+ * string of alternating '1's and '0's, always starting with a '1'. The length
+ * of the string should match the given integer.
+ *
+ * function stringyStrings(length: Number) => String
+ *
+ * Algorithm:
+ * initiate empty result
+ * loop from 0 to length, inclusive
+ *   if i is even, add '1' to the result
+ *   else add '2' to the result
+ * return result
+ */
+
+function stringyStrings(length) {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    i % 2 === 0 ? result += '1' : result += '0';
+  }
+  return result;
+}
+
+/**
+ * Fibonacci Number Location By Length
+ * Write a function that calculates and returns the i of the first
+ * Fibonacci number that has the number of digits specified by the argument.
+ * (The first Fibonacci number has an i of 1.)
+ * You may assume that the argument is always an integer greater than or equal to 2.
+ *
+ * function fibonacciLocation(length) => BigInt
+ *
+ * Algorithm:
+ * Initiate previous input, value, current value and i
+ * Loop while current value's length is under input value
+ * - Add previous value to the current value
+ * - Replace previous value to the current value
+ *
+ */
+
+function fibonacciLocation() {
+  const input = 10n;
+  let nextNum = 1n;
+  let prevNum = 0n;
+  let num = 1n;
+  let i = 1n;
+
+  while (true) {
+    nextNum = num + prevNum;
+    prevNum = num;
+    num = nextNum;
+    i++;
+    if (num.toString().length >= input) {
+      return i;
+    }
+  }
+}
+
+const output = fibonacciLocation(4);
+console.log(output.toString());
