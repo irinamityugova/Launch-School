6058a41353063342a02056831239101efda68049
Irina Mityugova <info@irinamityugova.me>
2023-01-27 17:04:48 +0000

    Exercise: Easy 5 - Find the Duplicate

--- js_101/exercises/easy-5.js
+++ js_101/exercises/easy-5.js
@@ -68,5 +68,82 @@ function union(a, b) {
   return copy;
 }
 
-const output = union([1, 3, 5], [3, 6, 9]);
+/**
+ * Halvsies
+ *
+ * Write a function that takes an array as an argument and
+ * returns an array that contains two elements, each of which
+ * is an array. Put the first half of the original array
+ * elements in the first element of the return value, and
+ * put the second half in the second element. If the
+ * original array contains an odd number of elements,
+ * place the middle element in the first half array.
+ *
+ * Ex: halvsies([1, 2, 3, 4]);       // [[1, 2], [3, 4]]
+ * Ex: halvsies([1, 5, 2, 4, 3]);    // [[1, 5, 2], [4, 3]]
+ *
+ * function halvsies(arr: Array) => Array
+ *
+ * Algorithm:
+ * Set elbow as the middle index to length of an array divided by 2 and ceil the decimal
+ * Set first to a slice of array from 0 index to the elbow (not included)
+ * Set second array from melbow to the end of the Array
+ * Return an array of first and second
+ */
+
+function halvsies(arr) {
+  const elbow = Math.ceil(arr.length / 2);
+  const first = arr.slice(0, elbow);
+  const second = arr.slice(elbow);
+  return [first, second];
+}
+
+/**
+ * Find the Duplicate
+ * Given an unordered array and the information that exactly one value in the array occurs twice
+ * (every other value occurs exactly once), determine which value occurs twice. Write a function
+ * that will find and return the duplicate value that is in the array.
+ *
+ * Ex: findDup([1, 5, 3, 1]);                                // 1
+ * findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+ *          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+ *          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+ *          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+ *          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+ *          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+ *          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+ *          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+ *          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+ *           7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
+ *
+ * function findDup(numbers: Array) => Number
+ *
+ * Algorithm:
+ * Loop through numbers with i index
+ * - Set slice to numbers at the next index to the end
+ * - Set currentNumber to the current number (labels are important)
+ * - If slice includes the current number, return the number
+ * After the loop, return "No duplicates"
+ *
+ */
+
+function findDup(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const slice = numbers.slice(i + 1); // Test more often. Here is a good spot
+    const currentNumber = numbers[i]; // Test for syntax errors here
+    if (slice.includes(numbers[i])) return numbers[i]; // Test if method works as expected
+  }
+  return 'No duplicates';
+}
+
+const output = findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+  38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+  14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+  78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+  89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+  41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+  55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+  85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+  40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+   7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
 console.log(output);
