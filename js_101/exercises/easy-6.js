b14930eaa7f812759bb548da9f8bd505708b9f70
Irina Mityugova <info@irinamityugova.me>
2023-01-28 06:12:44 +0000

    Exercise: Easy 6 - Double Char (Part 2)

--- js_101/exercises/easy-6.js
+++ js_101/exercises/easy-6.js
@@ -20,5 +20,32 @@
 
 const repeater = (str) => str.split('').map(e => e + e).join('');
 
-const output = repeater('Hello');        // "HHeelllloo"
+/**
+ * Double Char (Part 2)
+ * Write a function that takes a string, doubles every consonant character in
+ * the string, and returns the result as a new string. The function should not
+ * double vowels ('a','e','i','o','u'), digits, punctuation, or whitespace.
+ *
+ * Ex: doubleConsonants('String');          // "SSttrrinngg"
+ * Ex: doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
+ * Ex: doubleConsonants('July 4th');        // "JJullyy 4tthh"
+ * Ex: doubleConsonants('');                // ""
+ *
+ * function doubleConsonants(str: String) => String
+ *
+ * Algorithm:
+ * Split the string
+ * Loop characters
+ * If constant, double
+ * Join the split
+ * Return
+ */
+const doubleConsonants = (str) => str.split('').map((char) => {
+  if (char.match(/[^aeiou]/i)) {
+    return char.repeat(2)
+  }
+  return char;
+  }).join('');
+
+const output = doubleConsonants('String');          // "SSttrrinngg"
 console.log(output);
