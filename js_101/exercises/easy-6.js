b5013cc4c575ffe4df16d3d3837c92a79b9683ed
Irina Mityugova <info@irinamityugova.me>
2023-01-29 00:41:07 +0000

    Exercise: Easy 6 - Sequence Count

--- js_101/exercises/easy-6.js
+++ js_101/exercises/easy-6.js
@@ -80,5 +80,40 @@ function reverseNumber(num) {
   return Number(arr.join(''));
 }
 
-const output = reverseNumber(12345);    // 54321
+/**
+ * Sequence Count
+ *
+ * Create a function that takes two integers as arguments. The first argument is
+ * a count, and the second is the starting number of a sequence that your
+ * function will create. The function should return an array containing the same
+ * number of elements as the count argument. The value of each element should be
+ * a multiple of the starting number.
+ *
+ * You may assume that the count argument will always be an integer greater than
+ * or equal to 0. The starting number can be any integer. If the count is 0, the
+ * function should return an empty array.
+ *
+ * Ex: sequence(5, 1);          // [1, 2, 3, 4, 5]
+ * Ex: sequence(4, -7);         // [-7, -14, -21, -28]
+ * Ex: sequence(3, 0);          // [0, 0, 0]
+ * Ex: sequence(0, 1000000);    // []
+ *
+ * function sequence(count: Number, start: Number) => Array [...Number]
+ *
+ * Algorithm:
+ * Set an empty array named result
+ * Loop num statring at start, while result's length is less then count, increment by start
+ * - push num to the result
+ * Return result
+ */
+
+function sequence(count, start) {
+  const result = [];
+  for (let num = start; result.length < count; num += start) {
+    result.push(num);
+  }
+  return result;
+}
+
+const output = sequence(5, 1);          // [1, 2, 3, 4, 5]
 console.log(output);
