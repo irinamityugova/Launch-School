33827628cf4a012f9dc4124a4c96720620a912e5
Irina Mityugova <info@irinamityugova.me>
2023-04-14 21:12:35 +0000

    Refactoring for OOP

--- js_120/rock-paper-scissors.js
+++ js_120/rock-paper-scissors.js
@@ -1,7 +1,49 @@
-/**
- * Rock Paper Scissors
- *
- * Both the opponents choose an item from rock, paper, and scissors.
+/* Rock Paper Scissors */
+
+/*
+Object Oriented Refactoring
+
+1. Write a textual description of the problem or exercise.
+A player plays Rock Paper Scissors Lizard Spok against the computer.
+
+Rules of the game:
+"Scissors cuts Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+(and as it always has) Rock crushes Scissors"
+
+The result of the round gets displayed.
+
+2. Extract the significant nouns and verbs from the description.
+Nouns: Guide, Leaderboard, Round, Turn, Person, Computer, Moves: Rock (r) Paper (p) Scissors (s) Lizard (l) Spok (sp)
+Verbs: switchTurn, makeMove, getWinner, display
+
+3. Organize and associate the verbs with the nouns.
+RPSGame {
+
+  moves,
+  round,
+  humanMove();
+  computerMove();
+
+  guide,
+  leaderboard,
+  display(),
+
+  winner: user/computer/tie
+  getWinner(),
+
+}
+
+*/
+
+/** Both the opponents choose an item from rock, paper, and scissors.
  * The winner is decided according to the following rules:
  *
  * If player a chooses rock and player b chooses scissors, player a wins.
@@ -46,128 +88,164 @@
 
 import rlSync from 'readline-sync';
 
-const playRPS = function playRockPaperScissors() {
-  console.log(`
+function createPlayer(moves, playerType = 'bot', name = 'computer') {
+  const player = {
+    playerType,
+    name,
+    moves,
+    move: null,
+
+    choose() {
+      const choices = Object.keys(this.moves);
+
+      if (this.isHuman()) {
+        console.log(`Pick your move:${choices.map((choice, index) => `\n${index + 1} - ${choice}`)}`);
+        let index = -1;
+
+        while (index >= choices.length || index < 0 || isNaN(Number(index))) {
+          index = rlSync.question('> ') - 1;
+        }
+
+        const shortcut = choices[index];
+        this.move = moves[shortcut];
+
+        console.log(`You picked "${this.move.name}"`);
+      } else {
+        const index = parseInt((Math.random() * (choices.length)), 10);
+        const shortcut = choices[index];
+        this.move = moves[shortcut];
+
+        console.log(`Opponent picked "${this.move.name}".`);
+      }
+    },
+
+    isHuman() {
+      return this.playerType === 'human';
+    },
+  };
+  return player;
+}
+
+const round = {
+    time: Date.now(),
+    winner: null,
+    repeat: true,
+    moves: {
+    'r': {
+      wins: {
+        'lizard': 'smashes',
+        'scissors': 'crushes'
+      },
+      name: 'rock',
+    },
+    'p': {
+      wins: {
+        'rock': 'covers',
+        'spock': 'disproves'
+      },
+      name: 'paper',
+    },
+    's': {
+      wins: {
+        'paper': 'cuts',
+        'lizard': 'decapitates'
+      },
+      name: 'scissors',
+    },
+    'sp': {
+      wins: {
+        'rock': 'vaporizes',
+        'scissors': 'smashes'
+      },
+      name: 'spock',
+    },
+    'l': {
+      wins: {
+        'paper': 'eats',
+        'spock': 'poisons'
+      },
+      name: 'lizard',
+    },
+  },
+};
+
+const playRPS = {
+    guide: `
 |-------------------------------------------------|
 |         Welcome to Rock Paper Scissors!         |
 |-------------------------------------------------|
-| Pick a choice and win your opponent.            |
-| Rock beats scissors, scissors beat paper, and   |
-| paper beats rock. Good luck!                    |
+| Scissors cuts Paper                             |
+| Paper covers Rock                               |
+| Rock crushes Lizard                             |
+| Lizard poisons Spock                            |
+| Spock smashes Scissors                          |
+| Scissors decapitates Lizard                     |
+| Lizard eats Paper                               |
+| Paper disproves Spock                           |
+| Spock vaporizes Rock                            |
+| (and as it always has) Rock crushes Scissors    |
+|                                                 |
 | Press Ctrl-C to exit the application.           |
 |-------------------------------------------------|
-  `);
+  `,
 
-  const validateInput = function validateInput(choiceArray, userChoice) {
-    if (Number(userChoice) && +userChoice > 0 && +userChoice <= choiceArray.length) {
-      console.log(`You picked "${choiceArray[userChoice - 1]}"`);
-      return choiceArray[userChoice - 1];
+  history: [
+      {
+        winner: 'test',
+        time: 'testTime',
+        move: 'move',
+      },
+    ],
+
+  human: createPlayer(round.moves, 'human', 'Irina'),
+  bot: createPlayer(round.moves),
+
+  displayWinner() {
+    let humanMove = this.human.move.name;
+    let botMove = this.bot.move.name;
+    let humanWinMoves = this.human.move.wins;
+    let botWinMoves = this.bot.move.wins;
+
+    if(humanWinMoves.hasOwnProperty(botMove)) {
+      console.log(`You won! Your ${humanMove} ${humanWinMoves[botMove]} ${this.bot.name}'s ${botMove}`);
+      this['history'].push({
+        winner: this.human.name,
+        time: Date.now(),
+        move: humanMove
+      });
+    } else if(humanMove === botMove) {
+      console.log(`Whoops, ${humanMove}s knocked each other out!`);
+      this['history'].push({
+        winner: 'tie',
+        time: Date.now(),
+        move: humanMove
+      });
+    } else {
+      console.log(`Oh no! The ${this.bot.name}'s ${botMove} ${botWinMoves[humanMove]} your ${humanMove}`);
+      this['history'].push({
+        winner: this.bot.name,
+        time: Date.now(),
+        move: botMove
+      });
     }
-    if (choiceArray.includes(userChoice)) {
-      console.log(`You picked ${userChoice}`);
-      return userChoice;
-    }
-    console.log(`ERROR: ${userChoice} is invalid!
-Pick a number or the word from the list.`);
-    validateInput(choiceArray, rlSync.question('> ').toLowerCase());
-    return null;
-  };
+  },
 
-  const getUserChoice = function getUserChoice(choiceArray, label) {
-    console.log(`${label} ${choiceArray.map((e, i) => `\n${i + 1} - ${e}`)}`);
-    const userChoice = rlSync.question('> ').toLowerCase();
-    return validateInput(choiceArray, userChoice);
-  };
+  play() {
+    while (round.repeat) {
+      console.log(this.guide);
+      this.human.choose();
+      this.bot.choose();
+      this.displayWinner(this.human.move.name, this.bot.move.name);
 
-  const getBotChoice = function getBotChoice(choiceArray) {
-    const randomIndex = parseInt((Math.random() * (choiceArray.length)), 10);
-    const botChoice = choiceArray[randomIndex];
-    console.log(`Opponent picked "${botChoice}".`);
-    return botChoice;
-  };
-
-  const displayWinner = function displayWinner(userMove, botMove) {
-    switch (userMove) {
-      case botMove:
-        console.log(`Whoops, ${userMove}s knokced each other out!`);
-        break;
-
-      case 'rock':
-        if (botMove === 'scissors'
-          || botMove === 'lizard') {
-          console.log(`Your ${userMove} crushed the ${botMove}. Congrats!`);
-        } else if (botMove === 'spok') {
-          console.log(`Oh no, ${botMove} vaporizes your ${userMove}`);
-        } else if (botMove === 'paper') {
-          console.log(`Oh no, ${botMove} covers your ${userMove}`);
-        } break;
-
-      case 'paper':
-        if (botMove === 'rock') {
-          console.log(`Your ${userMove} covers the ${botMove}. Congrats!`);
-        } else if (botMove === 'spock') {
-          console.log(`Yes! Invincible ${userMove} disproves ${botMove}. Congrats!`);
-        } else if (botMove === 'lizard') {
-          console.log(`Oh no, ${botMove} eats your ${userMove}...`);
-        } else if (botMove === 'scissors') {
-          console.log(`Oh no, ${botMove} cuts your ${userMove}...`);
-        } break;
-
-      case 'scissors':
-        if (botMove === 'paper') {
-          console.log(`Your ${userMove} cuts the ${botMove}. Congrats!`);
-        } else if (botMove === 'lizard') {
-          console.log(`Your ${userMove} decapitates the ${botMove}. Congrats!`);
-        } else if (botMove === 'spock') {
-          console.log(`Oh no, ${botMove} smashes your ${userMove}...`);
-        } else if (botMove === 'rock') {
-          console.log(`Oh no, ${botMove} crushes your ${userMove}...`);
-        } break;
-
-      case 'spock':
-        if (botMove === 'rock') {
-          console.log(`Nice job, ${userMove} vaporizes the ${botMove}!`);
-        } else if (botMove === 'scissors') {
-          console.log(`Yep, ${userMove} smashes the ${botMove}!`);
-        } else if (botMove === 'lizard') {
-          console.log(`Oh no, ${botMove} poisons ${userMove}...`);
-        } else if (botMove === 'paper') {
-          console.log(`Oh no, ${botMove} disproves ${userMove}...`);
-        } break;
-
-      case 'lizard':
-        if (botMove === 'paper') {
-          console.log(`Your ${userMove} covers the ${botMove}. Congrats!`);
-        } else if (botMove === 'spock') {
-          console.log(`Your ${userMove} covers the ${botMove}. Congrats!`);
-        } else if (botMove === 'scissors') {
-          console.log(`Oh no, ${botMove} decapitates ${userMove}...`);
-        } else if (botMove === 'rock') {
-          console.log(`Oh no, ${botMove} crushes ${userMove}...`);
-        } break;
-
-      default:
-        console.log(`ERROR: Couldn't compute
-        userMove = ${userMove}, botMove = ${botMove}`);
-    }
-    return null;
-  };
-
-  const gameLoop = function gameLoop() {
-    const moves = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
-    while (true) {
-      const userMove = getUserChoice(moves, 'Make your move:');
-      const botMove = getBotChoice(moves);
-      displayWinner(userMove, botMove);
-
-      const playAgain = getUserChoice(['yes', 'no'], 'Play again?');
-      if (playAgain === 'no') {
-        console.log('Goodbye!');
-        return;
+      let playAgain = rlSync.question('Play again? yes/no\n> ');
+      if (playAgain.toLowerCase().match(/n/i)) {
+        round.repeat = false;
       }
+      console.clear();
     }
-  };
-  gameLoop();
+    console.log('Goodbye!');
+    return "Goodbye!";
+  },
 };
 
-playRPS();
+playRPS.play();
