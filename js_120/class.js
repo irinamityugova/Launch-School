0f8717a35ec80d9ee9d19e3102205df162d6345e
Irina Mityugova <info@irinamityugova.me>
2023-05-14 21:12:03 +0000

    Mixin practice

---
+++ js_120/class.js
@@ -0,0 +1,29 @@
+/* If we have a Car class and a Truck class, how can you use the Speed object as a mix-in to make them goFast? How can you check whether your Car or Truck can now go fast?
+*/
+
+const Speed = {
+  goFast() {
+    console.log(`I'm a ${this.constructor.name} and going super fast!`);
+  }
+};
+
+class Car {
+  goSlow() {
+    console.log(`I'm safe and driving slow.`);
+  }
+}
+Object.assign(Car.prototype, Speed);
+
+class Truck {
+  goVerySlow() {
+    console.log(`I'm a heavy truck and like going very slow.`);
+  }
+}
+
+Object.assign(Truck.prototype, Speed);
+
+const ford = new Truck();
+const honda = new Car();
+
+ford.goFast();
+honda.goFast();
