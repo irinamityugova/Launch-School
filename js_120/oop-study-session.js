529bc5adfadbb366dca81080af9555cc5747ae35
Irina Mityugova <info@irinamityugova.me>
2023-05-19 00:53:39 +0000

    Study Session Homework

---
+++ js_120/oop-study-session.js
@@ -0,0 +1,18 @@
+/*
+Create an OOP skeletal design based on these requirements. You can use any object creation pattern. Think carefully about the relationships being created between the types and try to aim for the least amount of repetition possible. There are several correct answers.
+
+We have a role-playing game that features several enemies that are  mythical creatures or some other type:
+
+- We have Horses that can gallop.
+    - A Pegasus is a type of Horse that can gallop and fly.
+    - A Unicorn is a type of Horse that can gallop, and pierce.
+        - Unicorns are known to be very rare. We should have a method defined somewhere in our code that lets us know how many unicorns have ever been created.
+
+- We have Humans that can speak.
+    - A Thief is a type of Human that can steal.
+    - A Pirate is a type of Thief that can steal and sail.
+- A Wizard is a type of Human that can cast spells.
+
+- A Centaur is a creature that has a torso of a man, and the legs of a horse. They can speak and gallop. They also have the ability to slash with their swords.
+
+*/
