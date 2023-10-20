e8ea42345e5b3630c5555f9b8a10e316bd83997f
Irina Mityugova <info@irinamityugova.me>
2023-10-20 00:10:26 +0000

    Reinventing reduce method

---
+++ js_130/reduce.js
@@ -0,0 +1,30 @@
+function reduce(arr, cb, accum) {
+  let i = 0;
+
+  if(arr[0] && ! accum) {
+      accum = arr[0];
+      i = 1;
+  }
+
+  for (i; i < arr.length; i++) {
+    let curr = arr[i];
+    accum = cb(accum, curr);
+  }
+
+  return accum;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(reduce(numbers, (accum, number) => accum + number));   // => 15
+console.log(reduce(numbers, (prod, number) => prod * number));     // => 120
+console.log(reduce(numbers, (prod, number) => prod * number, 3));  // => 360
+console.log(reduce([], (accum, number) => accum + number, 10));    // => 10
+console.log(reduce([], (accum, number) => accum + number));
+// => undefined
+
+let stooges = ["Mo", "Larry", "Curly"];
+console.log(reduce(stooges, (reversedStooges, stooge) => {
+  reversedStooges.unshift(stooge);
+  return reversedStooges;
+}, []));
+// => ["Curly", "Larry", "Mo"]
