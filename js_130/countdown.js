db634bf669956f4c5524bb8faf30ef92dc33f493
Irina Mityugova <info@irinamityugova.me>
2023-10-23 06:41:32 +0000

    Added recursive solution

--- js_130/countdown.js
+++ js_130/countdown.js
@@ -1,6 +1,13 @@
-((num) => {
-  while (num > 0) {
-    console.log(num);
-    num--;
-  }
+// ((num) => {
+//   while (num > 0) {
+//     console.log(num);
+//     num--;
+//   }
+// })(7);
+
+// Add a recursive solution
+
+(function countdown(num) {
+  console.log(num);
+  if (num > 0) return countdown(num - 1);
 })(7);
