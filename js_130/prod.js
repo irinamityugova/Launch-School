15876e8d2ccdbb1d33091a0f58ab5545c3e11c31
Irina Mityugova <info@irinamityugova.me>
2023-10-23 06:34:51 +0000

    Added a recursive solution to the product problem

---
+++ js_130/prod.js
@@ -0,0 +1,32 @@
+// For an extra challenge, refactor the solution to this problem using recursion. Bear in mind that named functions created as IIFEs can be referenced by those same functions. That is, you can call a function's name in the body of the IIFE. Don't worry if you can't solve this problem; it's a bit tricky.
+
+// let bar = (function(start) {
+//   let prod = start;
+//   return function (factor) {
+//     prod *= factor;
+//     return prod;
+//   };
+// })(2);
+
+// let result = bar(3);
+// result += bar(4);
+// result += bar(5);
+// console.log(result);
+
+let bar = (function foo(start) {
+  let prod = start;
+
+  return function bar(factor) {
+    console.log('f =', factor);
+    if (factor === 1) {
+      return prod;
+    }
+    prod += bar(factor - 1);
+    return prod;
+  };
+})(2);
+
+let result = bar(3); // 6
+result += bar(4); // 24
+result += bar(5); // 120
+console.log(result); // 150
