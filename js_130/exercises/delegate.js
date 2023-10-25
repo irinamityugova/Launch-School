c04c280041cba80a51c59957de24612028ee0406
Irina Mityugova <info@irinamityugova.me>
2023-10-25 04:01:59 +0000

    Testing out the old way of converting arguments object into an array

--- js_130/exercises/delegate.js
+++ js_130/exercises/delegate.js
@@ -1,6 +1,7 @@
 // Write a delegate function that can be used to delegate the behavior of a method or function to another object's method. delegate takes a minimum of two arguments: (1) the object and (2) name of the method on the object. The remaining arguments, if any, are passed — as arguments — to the objects' method that it delegates to.
 
-function delegate(obj, prop, ...args) {
+function delegate(obj, prop) {
+  let args = Array.prototype.slice.call(arguments, 2);
   return function() {
     obj[prop].call(obj, args);
   };
