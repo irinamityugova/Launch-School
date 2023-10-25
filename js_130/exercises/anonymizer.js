356036f27e433ed210af57560ff7fe41cd2a62b1
Irina Mityugova <info@irinamityugova.me>
2023-10-25 05:28:24 +0000

    Refactoring using an IIFE

--- js_130/exercises/anonymizer.js
+++ js_130/exercises/anonymizer.js
@@ -75,15 +75,21 @@ let Account = (function() {
 })();
 
 let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
-
 console.log(fooBar.firstName);                     // returns the firstName function
 console.log(fooBar.email);                         // returns the email function
 console.log(fooBar.firstName('123456'));           // logs 'foo'
 console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
 console.log(fooBar.displayName);                   // logs 16 character sequence
-console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password';
+console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password'
 console.log(fooBar.resetPassword('123456', 'abc')) // logs true
 
 let displayName = fooBar.displayName;
 fooBar.reanonymize('abc');                         // returns true
 console.log(displayName === fooBar.displayName);   // logs false
+
+let bazQux = Object.create(Account).init('baz@qux.com', '123456', 'baz', 'qux');
+// Note that the following two lines of code are correct as written.
+// There are no mistakes. We are attempting to demonstrate that the
+// code does not work as might be intended.
+console.log(fooBar.firstName('123456'));           // logs 'baz' but should log foo.
+console.log(fooBar.email('123456'));               // 'baz@qux.com' but should 'foo@bar.com'
